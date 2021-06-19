870a79afa1a0ae4ee8d7aef546e04b54a9cf675b
arascic1 <72926552+arascic1@users.noreply.github.com>
2021-06-19 22:14:34 +0200

    Delete CODE/APIHandler directory

--- CODE/APIHandler/Main.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.us;
-
-public class Main {
-    public static void main(String[] args) {
-        APIHandler API = new APIHandler();
-        System.out.println(API.getKanalFromUsername("MotoGP"));
-        System.out.println(API.getKanalFromChannelID("UCq0j6FS1V1mit68TtRFxQUQ"));
-    }
-}
